f857a41e5c6ee13670590cae5429816d76730295
phongdk29101999 <phong.dk176843@sis.hust.edu.vn>
2022-06-22 20:18:55 +0700

    init project

---
+++ app/Http/Controllers/ListsController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Auth;
+use App\Lists;
+use App\Board;
+
+class ListsController extends Controller {
+    public function index($board_id) {
+        $list = DB::select( DB::raw("SELECT id, title, board_id FROM lists WHERE board_id = '$board_id'") );
+        for($i=0; $i<count($list); $i++){
+            $list_id = $list[$i]->id;
+            $list[$i]->card = DB::select( DB::raw("SELECT id, title, dead_line, status FROM tasks WHERE lists_id = '$list_id'") );
+        }
+
+        return response()->json($list);
+    }
+
+    public function saveList(Request $request, $board_id) {
+        $id = DB::table('lists')->insertGetId(
+            ['title' => $request->title,'board_id'=>$board_id]);
+        return response()->json(["id"=>$id, "title"=>$request->title]);
+    }
+
+    public function findOne($id, $list_id) {
+        $list = Lists::find($list_id);
+        return response()->json($list);
+    }
+
+    public function updateList(Request $request, $id, $list_id) {
+        $list = Lists::find($list_id);
+        if (isset($request->index)){
+            $list->index = $request->index;
+        }
+        if (isset($request->title)){
+            $list->title = $request->title;
+        }
+        if (isset($request->id)){
+            $list->id=1000;
+        }
+        $result = $list->save();
+        if ($result) {
+            return response()->json(['status' => 'success', 'message' => 'list updated successfully']);
+        } else {
+            return response()->json(['status' => 'fail', 'message' => 'list updated failure']);
+        }
+    }
+
+    public function deleteList($id, $list_id) {
+        $list = Lists::find($list_id);
+        $result = $list->delete();
+        if ($result) {
+            return response()->json(['status' => 'success', 'message' => 'list deleted successfully']);
+        } else {
+            return response()->json(['status' => 'fail', 'message' => 'list deleted failure']);
+        }
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Index</title>
+	<link href="{{ asset('assets/css/general.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/css/index.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/fontawesome/css/all.min.css') }}" rel="stylesheet">
+</head>
+
+<body>
+	<div class="root">
+		<div class="nav">
+			<div class="logo">
+				<i class="fas fa-jedi"></i>
+				<span style="margin-left: 10px">Nhóm 12</span>
+			</div>
+			<div style="height: 100%;flex-grow: 1;"></div>
+			<div class="btn-control">
+				<a href="/login" id="login" class="login btn">
+					<span>Login</span>
+				</a>
+				<a href="/register" id="signup" class="signup btn">
+					<span>Sign Up</span>
+				</a>
+			</div>
+		</div>
+
+		<div class="introduce">
+			<div class="content">
+				<div>
+					<h1>Trello helps teams work more collaboratively and get more done.</h1>
+					<p>Trello’s boards, lists, and cards enable teams to organize and prioritize projects in a fun,
+						flexible, and rewarding way.</p>
+				</div>
+			</div>
+			<div class="illustration">
+			</div>
+		</div>
+	</div>
+	<div class="our-team">
+		<header>
+			<h2>NHÓM 12</h2>
+		</header>
+		<main>
+			<ul>
+				<li>
+					<div class="our-team-img">
+						<img src="\assets\img\thang.jpg" alt="" >
+					</div>
+					<div class="our-team-text">
+						<h3>NGUYEN MINH THANG</h3>
+						<div class="info">
+							<p> MSSV:20173306</p>
+							<P>LEADER</P>
+						</div>
+						<div class="social">
+							<i class="fab fa-facebook-f"></i>
+							<i class="fab fa-twitter"></i>
+							<i class="fab fa-instagram"></i>
+							<i class="far fa-envelope"></i>
+						</div>
+					</div>
+				</li>
+				<li>
+				<div class="our-team-img">
+					<img src="\assets\img\avatar.jpg" alt="">
+				</div>
+				<div class="our-team-text">
+					<h3>DUONG XUAN HOAN</h3>
+					<div class="info">
+						<P>MSSV:20173125 </P>
+						<p>MEMBER</p>
+					</div>
+					<div class="social">
+						<i class="fab fa-facebook-f"></i>
+						<i class="fab fa-twitter"></i>
+						<i class="fab fa-instagram"></i>
+						<i class="far fa-envelope"></i>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="our-team-img">
+					<img src="\assets\img\avatar.jpg" alt="">
+				</div>
+				<div class="our-team-text">
+					<h3>VUONG VAN AN</h3>
+					<div class="info">
+						<p>MSSV:20172932 </p>
+						<p>MEMBER</p>
+					<div class="social">
+						<i class="fab fa-facebook-f"></i>
+						<i class="fab fa-twitter"></i>
+						<i class="fab fa-instagram"></i>
+						<i class="far fa-envelope"></i>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="our-team-img">
+					<img src="\assets\img\avatar.jpg" alt="">
+				</div>
+				<div class="our-team-text">
+					<h3>NGUYEN DUC MANH HOANG</h3>
+					<div class="info">
+						<P>MSSV:20173139</P>
+						<p>MEMBER</p>
+					</div>
+					<div class="social">
+						<i class="fab fa-facebook-f"></i>
+						<i class="fab fa-twitter"></i>
+						<i class="fab fa-instagram"></i>
+						<i class="far fa-envelope"></i>
+					</div>
+				</div>
+			</li>
+			</ul>
+			
+		</main>
+	</div>
+</body>
+
+</html>
